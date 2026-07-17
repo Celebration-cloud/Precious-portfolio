aa2eba924df4150f85445d82c1ab762056c70071
Celebration-cloud <ojinguluc@gmail.com>
2026-07-17 11:52:45 +0100

    feat: change contributor

--- app/studio/[[...tool]]/layout.tsx
+++ app/studio/[[...tool]]/layout.tsx
@@ -10,5 +10,5 @@ export default function StudioLayout({ children }: { children: React.ReactNode }
       <script src={DASHBOARD_BRIDGE_SCRIPT} async type="module" />
       {children}
     </div>
-  ); 
+  );
 }
